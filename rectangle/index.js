9297f86234b492cec318b5612978c190a9546d2a
Mike McDonald <whoiswizard@icloud.com>
2021-03-17 18:07:08 -0500

    finished rectangle

--- rectangle/index.js
+++ rectangle/index.js
@@ -3,7 +3,58 @@ const { subscribe, dispatch, getState } = createStore(reducer);
 
 subscribe(() => render(getState()));
 
-dispatch({type: null}); // Here we're making a call to dispatch() - this triggers the first render.
+dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
-// Write DOM event listeners here, make them dispatch actions to the Redux store 
+// Write DOM event listeners here, make them dispatch actions to the Redux store
 
+const widthBtn = document.getElementById("width");
+const heightBtn = document.getElementById("height");
+const redBtn = document.getElementById("red");
+const blueBtn = document.getElementById("blue");
+
+// +10 Width
+widthBtn.addEventListener("click", () => {
+  console.log("WIDE");
+  let action = {
+    type: "CHANGE_SIZE",
+    payload: {
+      attr: "width",
+      value: 10,
+    },
+  };
+  dispatch(action);
+});
+// +10 Height
+heightBtn.addEventListener("click", () => {
+  console.log("HIGH");
+  let action = {
+    type: "CHANGE_SIZE",
+    payload: {
+      attr: "height",
+      value: 10,
+    },
+  };
+  dispatch(action);
+});
+// Change Color - Red
+redBtn.addEventListener("click", () => {
+  console.log("RED");
+  let action = {
+    type: "CHANGE_COLOR",
+    payload: {
+      color: "red",
+    },
+  };
+  dispatch(action);
+});
+// Change Color - Blue
+blueBtn.addEventListener("click", () => {
+  console.log("BLUE");
+  let action = {
+    type: "CHANGE_COLOR",
+    payload: {
+      color: "blue",
+    },
+  };
+  dispatch(action);
+});
--- rectangle/reducer.js
+++ rectangle/reducer.js
@@ -1,18 +1,28 @@
 const initialState = {
-    width: 100,
-    height: 200,
-    color: "#FF00FF"
+  width: 100,
+  height: 200,
+  color: "#FF00FF",
 };
 
 const reducer = (state = initialState, action) => {
-    // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+  // Handle actions here - make sure you don't mutate the state!
+  const { type } = action;
+  switch (type) {
+    case "CHANGE_SIZE": // Trying to shrink to one case"update rectangle" or two "change size" + "change Color"
+    console.log(action)  ;
+    return {
+        ...state,
+        [action.payload.attr]:
+          state[action.payload.attr] + action.payload.value,
+      };
+    case "CHANGE_COLOR":
+      return {
+        ...state,
+        color: action.payload.color,
+      };
 
-    // ACTION 1 - Increment height by 10
+    default:
+      return state;
+  }
+};
 
-    // ACTION 2 - Increment width by 10
-
-    // ACTION 3 - Change the color
-
-    return state;
-}

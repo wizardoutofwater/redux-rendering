88e365a7044dc97720c7afa6f1996444609d0e4a
Mike McDonald <whoiswizard@icloud.com>
2021-03-17 13:22:39 -0500

    finished poker

--- poker/index.js
+++ poker/index.js
@@ -6,4 +6,30 @@ subscribe(() => render(getState()));
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
+const values = [2,3,4,5,6,7,8,9,10,'A','K','Q','J'];
+const suits = ['C','D','H','S'];
 
+const randomValue = () => values[Math.floor(Math.random() * values.length)];
+const randomSuit = () => suits[Math.floor(Math.random() * suits.length)];
+
+const aCardBtn = document.getElementById('card1Button')
+const bCardBtn = document.getElementById('card2Button')
+
+aCardBtn.addEventListener("click", () => {
+    console.log("button 1 pressed")
+
+    dispatch({
+        type: "RANDOM_CARD_A",
+        newValue: randomValue(),
+        newSuit: randomSuit()
+    })
+})
+
+bCardBtn.addEventListener("click", () => {
+    
+    dispatch({
+        type: "RANDOM_CARD_B",
+        newValue: randomValue(),
+        newSuit: randomSuit()
+    })
+})
--- poker/reducer.js
+++ poker/reducer.js
@@ -1,19 +1,39 @@
 const initialState = [
-    {
-        value: "K",
-        suit: "C"
-    },
-    {
-        value: "K",
-        suit: "D"
-    }
+  {
+    value: "K",
+    suit: "C",
+  },
+  {
+    value: "K",
+    suit: "D",
+  },
 ];
 
 const reducer = (state = initialState, action) => {
-    // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+  // Handle actions here - make sure you don't mutate the state!
+  const { type, newValue, newSuit } = action;
 
-    // ACTION - Change either card to a random new one (Try implementing one action that both buttons dispatch)
-    
-    return state;
-}
+  switch (type) {
+    case "RANDOM_CARD_A":
+      return [
+        {
+          value: newValue,
+          suit: newSuit,
+        },
+        ...state.slice(1),
+      ]
+
+    case "RANDOM_CARD_B":
+      return [
+        ...state.slice(0, 1),
+        {
+          value: newValue,
+          suit: newSuit,
+        },
+      ]
+    default:
+      return state;
+  }
+};
+
+
